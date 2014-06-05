6b42a0b7bca922fc0f9f8dd334960e1390ec10a7
Dennis Wiebusch <dennis.wiebusch@uni-wuerzburg.de>
2014-06-05 19:58:48 +0200

    update to new version

--- project/Build.scala
+++ project/Build.scala
@@ -2,28 +2,77 @@ import sbt._
 import Keys._
 
 object SirisProductionBuild extends Build {
-  
-  object BuildSettings {
-    val buildSettings = Defaults.defaultSettings ++ Seq(
-      organization := "The Siris Team",
-      version := "1.0",
-      scalaVersion := "2.10.2"
-    )
+
+  val buildSettings = Defaults.defaultSettings ++ Seq(
+    organization := "The Siris Team",
+    version := "1.0",
+    scalaVersion := "2.11.0"
+  )
+
+  val simxBase = file(".")
+  //TODO: Check for thread safety in unusual cases(e.g. when compiling multiple SimXApplications at once)
+  var workingDirectory: File = file(".")
+
+  lazy val generateOntology = taskKey[Unit]("Generates the ontology for the current SimX application project.")
+  lazy val ontologyGen = Project(
+    id = "ontology-generator",
+    base = file( "components/ontology/generating"),
+    settings = buildSettings ++ Seq(
+      generateOntology := {
+        toError((runner in Compile).value.run(
+          mainClass = mainClass.value.getOrElse(throw new Exception("No 'mainClass' defined in build.sbt.")),
+          classpath = (fullClasspath in Compile).value.map(_.data),
+          options = Seq(simxBase.getAbsolutePath, workingDirectory.getAbsolutePath), //program arguments
+          log = streams.value.log)
+        )
+      }
+  ))
+
+  lazy val callOntoGen = taskKey[Unit]("Calls the ontology generation task.")
+
+  lazy val core = Project(
+    id = "core",
+    base = file( "core"),
+    settings = buildSettings ++ Seq(
+      callOntoGen := {(generateOntology in ontologyGen).value},
+      (compile in Compile) <<= (compile in Compile) dependsOn callOntoGen
+  ))
+
+  object SimXComponent {
+  	def apply(id: String, base: File) = Project(id, base, settings = buildSettings)
   }
-  
-  object SimXProject {
-  	def apply(id: String, base: File) = Project(id = id, base = base, settings = BuildSettings.buildSettings)
+
+  lazy val setOntoDir =
+    taskKey[Unit]("Sets the working directory for the ontology generation task to the projects baseDirectory.")
+
+  object SimXApplication {
+  	def apply(id: String, base: File) = Project(id, base, settings = buildSettings ++ Seq(
+      setOntoDir := {workingDirectory = baseDirectory.value},
+      (compile in Compile) <<= (compile in Compile) dependsOn setOntoDir
+    ))
   }
-  
-  lazy val core 	      = SimXProject ( id = "core",		    base = file( "core" ))
-  lazy val ontologygen    = SimXProject ( id = "ontologygen",   base = file( "components/ontology/generating"))
-  lazy val jbullet 	      = SimXProject ( id = "jbullet", 		base = file( "components/physics/jbullet" )) 	dependsOn( core )
-  lazy val jvr            = SimXProject ( id = "jvr", 		    base = file( "components/renderer/jvr"))    	dependsOn( core )
-  lazy val lwjgl_sound    = SimXProject ( id = "lwjgl_sound", 	base = file( "components/sound/lwjgl-sound"))   dependsOn( core )
-  lazy val vrpn           = SimXProject ( id = "vrpn", 		    base = file( "components/io/vrpn")) 		    dependsOn( core )
-  lazy val editor         = SimXProject ( id = "editor", 		base = file( "components/editor")) 	   	        dependsOn( core )
-  lazy val basicexamples  = SimXProject ( id = "basicexamples",	base = file( "applications/basicexamples"))	    dependsOn( core, jbullet, jvr, editor, ontologygen, lwjgl_sound )
-  lazy val simthief	      = SimXProject ( id = "simthief",		base = file( "applications/simthief"))		    dependsOn( core, jbullet, jvr, editor, vrpn, lwjgl_sound )
-  lazy val doc            = SimXProject ( id = "doc", 		    base = file( "doc")) 		                    dependsOn( core, jbullet, jvr, lwjgl_sound, vrpn, editor, basicexamples, simthief ) 
+
+  /* Components */
+   //Editor
+  lazy val editor         = SimXComponent ( id = "editor", 		         base = file( "components/editor")).
+                              dependsOn( core ).aggregate( core )
+  //IO
+  lazy val vrpn           = SimXComponent ( id = "vrpn", 		           base = file( "components/io/vrpn")).
+                              dependsOn( core ).aggregate( core )
+  //Physics
+  lazy val jbullet 	      = SimXComponent ( id = "jbullet", 		       base = file( "components/physics/jbullet" )).
+                              dependsOn( core ).aggregate(core)
+  //Renderer
+  lazy val jvr            = SimXComponent ( id = "jvr", 		           base = file( "components/renderer/jvr")).
+                              dependsOn( core ).aggregate( core )
+  //Sound
+  lazy val lwjgl_sound    = SimXComponent ( id = "lwjgl_sound",        base = file( "components/sound/lwjgl-sound")).
+                              dependsOn( core ).aggregate( core )
+   /* Applications*/
+  lazy val basicexamples  = SimXApplication ( id = "examples-basic",     base = file( "applications/examples/basic")).
+                              dependsOn(core, jbullet, jvr, lwjgl_sound, editor, vrpn).
+                              aggregate(core, jbullet, jvr, lwjgl_sound, editor, vrpn)
+  /* Documentation */
+  lazy val doc            = SimXComponent ( id = "doc", 		           base = file( "doc")).
+                              dependsOn( core, jbullet, jvr, lwjgl_sound, vrpn, basicexamples )
 }
-	
